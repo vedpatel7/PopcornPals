d1de8571c18bac3deb1c0c423999a74dd5869d05
vedpatel7 <vedvirpara2004@gmail.com>
2023-08-23 23:52:16 +0530

    youtube api fetch done

--- login-register-backend/index.js
+++ login-register-backend/index.js
@@ -13,11 +13,6 @@ app.use(cors()); // Enable CORS for cross-origin requests
 app.use(express.static(path.join(__dirname, "public")));
 
 
-const videoFileMap={
-    'cdn':'videos/cdn.mp4',
-    'generate-pass':'videos/generate-pass.mp4',
-    'get-post':'videos/get-post.mp4',
-}
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB"
 , {
@@ -89,43 +84,6 @@ app.post("/register", (req,res)=>{
         console.log(err)
     })
    
-})
-
-app.get('/videos/:filename', (req, res)=>{
-    const fileName = req.params.filename;
-    const filePath = videoFileMap[fileName]
-    if(!filePath){
-        return res.status(404).send('File not found')
-    }
-
-    const stat = fs.statSync(filePath);
-    const fileSize = stat.size;
-    const range = req.headers.range;
-
-    if(range){
-        const parts = range.replace(/bytes=/, '').split('-')
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-        const chunksize = end - start + 1;
-        const file = fs.createReadStream(filePath, {start, end});
-        const head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4'
-        };
-        res.writeHead(206, head);
-        file.pipe(res);
-    }
-    else{
-        const head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4'
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(filePath).pipe(res)
-    }
 })
 
 
--- login-register/src/components/homepage/homepage.js
+++ login-register/src/components/homepage/homepage.js
@@ -1,28 +1,47 @@
-import React, {useRef, useEffect} from "react"
+import React, { useRef, useEffect, useState } from "react"
 import "./homepage.css"
+import axios from 'axios';
 
-const Homepage =({setLoginUser},{videoId})=>
-{
-    const videoRef = useRef(null)
+const API="AIzaSyCqfE6uk9T8YT0pJTc36bRKK1fdDOLUYoo";
+const search="movie trailer"
+var fetchurl=`https://www.googleapis.com/youtube/v3/search?key=${API}&q=${search}&maxResult=20`
+const Homepage = ({ setLoginUser }) => {
+  const[allvideos,setAllvideos]=useState([])
+  useEffect(()=>{
+      axios.get(fetchurl)
+      .then(response => {
+        const result = response.data.items.map(item => ({
+          ...item,
+          Videolink: "https://www.youtube.com/embed/" + item.id.videoId
+        }));
+        setAllvideos(result);
+      })
+      .catch(error => {
+        console.error("Error fetching videos:", error);
+      });
+      
+  },[])
 
-    useEffect(()=>{
-        if(videoRef.current){
-            videoRef.current.pause()
-            videoRef.current.removeAttribute('src')
-            videoRef.current.load()
-        }
-    })
-
-    return(
-        <div className="homepage">
-        <h1>Hello Homepage</h1>
-        <video ref={videoRef} width='320' height='240' controls autoPlay>
-        <source src={`http://localhost:3000/videos/${videoId}`} type='video/mp4'></source>
-        Your browser does not support the video tag.
-        </video>
-        <div className="button" onClick={() => setLoginUser({})} >Logout</div>
+  console.log(allvideos);
+  return (
+    <div className="homepage">
+      <h1>Hello Homepage</h1>
+      <div>
+        <h1>Video List</h1>
+        <div>
+          {allvideos.map((item)=>{
+            return(
+              <div>
+              <iframe width="560" height="315" src={item.Videolink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              </div>
+            );
+          })}
         </div>
-    )
+      </div>
+
+      <div className="button" onClick={() => setLoginUser({})} >Logout</div>
+    </div>
+  )
 }
 
 export default Homepage;
