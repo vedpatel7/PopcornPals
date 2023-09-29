637d2e7d892f67e1353956641626f0ac7f2a84ec
vedpatel7 <vedvirpara2004@gmail.com>
2023-09-29 17:20:36 +0530

    new file:   client/src/components/watchlist/WatchList.css 	modified:   client/src/components/watchlist/WatchList.jsx

--- client/src/components/watchlist/WatchList.jsx
+++ client/src/components/watchlist/WatchList.jsx
@@ -1,44 +1,77 @@
 import axios from 'axios';
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import "./WatchList.css"; 
 
-const WatchList=()=>{
+const API_KEY = '84edaeedd68b9e73abbe95b5bb70617a';
+const Base_URL = 'https://api.themoviedb.org/3';
+
+const WatchList = () => {
   const navigate = useNavigate();
-    const { emailId } = useParams();
-    const [movieIds, setMovieIds] = useState([]);
-  
-    useEffect(() => {
-      axios.get(`http://localhost:9002/watchlist/${emailId}`)
-        .then(response => {
-          setMovieIds(response.data);
-          console.log(response)
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }, [emailId]);
+  const { emailId } = useParams();
+  const [movieIds, setMovieIds] = useState([]);
+  const [movieDetails, setMovieDetails] = useState([]);
 
-    const removeFromWatchlist = (movieIdToRemove) => {
-      axios.delete(`http://localhost:9002/watchlist/${emailId}/${movieIdToRemove}`)
-        .then(response => {
-          setMovieIds(prevMovieIds => prevMovieIds.filter(id => id !== movieIdToRemove));
-        })
+  useEffect(() => {
+    axios.get(`http://localhost:9002/watchlist/${emailId}`)
+      .then(response => {
+        setMovieIds(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [emailId]);
+
+  useEffect(() => {
+    const fetchMovieDetails = () => {
+      const promises = movieIds.map(movieId => {
+        return axios.get(`${Base_URL}/movie/${movieId}?api_key=${API_KEY}`);
+      });
+
+      axios.all(promises)
+        .then(axios.spread((...responses) => {
+          const movieDetails = responses.map(response => response.data);
+          const filteredMovieDetails = movieDetails.filter(movie => movie !== null);
+          setMovieDetails(filteredMovieDetails);
+        }))
         .catch(err => {
           console.log(err);
         });
     };
 
-    return(
-        <div>
-        <ul>
-        {movieIds.map((movieId, index) => (
-          <li key={index}  onClick={()=>navigate(`/searchMoviebyName/${movieId}`)}>{movieId}<button onClick={() => removeFromWatchlist(movieId)}>Remove</button></li>
+    fetchMovieDetails();
+  }, [movieIds]);
+
+  const removeFromWatchlist = (movieIdToRemove) => {
+    axios.delete(`http://localhost:9002/watchlist/${emailId}/${movieIdToRemove}`)
+      .then(response => {
+        setMovieIds(prevMovieIds => prevMovieIds.filter(id => id !== movieIdToRemove));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className='Watchlist'>
+      <ul>
+        {movieDetails.map((movieDetail, index) => (
+          <li key={index}>
+            <div className="movie-card" onClick={() => navigate(`/searchMoviebyName/${movieDetail.id}`)}>
+              <div className="button-container">
+                <button onClick={() => removeFromWatchlist(movieDetail.id)}>Remove</button>
+              </div>
+              <img src={`https://image.tmdb.org/t/p/w440_and_h660_face/${movieDetail.poster_path}`} alt={movieDetail.title} />
+              <div className="movie-details">
+                <h1 className="movie-title">{movieDetail.title}</h1>
+                <p className="movie-release-date">{movieDetail.release_date}</p>
+              </div>
+            </div>
+          </li>
         ))}
       </ul>
-        </div>
-    )
+    </div>
+  );
 }
 
 export default WatchList;
-
-// https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}
