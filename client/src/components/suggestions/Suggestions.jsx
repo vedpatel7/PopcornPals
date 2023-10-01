d21901645e339056262953fb0da56faf6a1e1c7a
vedpatel7 <vedvirpara2004@gmail.com>
2023-10-01 17:33:54 +0530

    added movie detials to the suggestion page

--- client/src/components/suggestions/Suggestions.jsx
+++ client/src/components/suggestions/Suggestions.jsx
@@ -1,29 +1,69 @@
 import axios from 'axios';
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
-const Suggestion=(props)=>{
-  const navigate= useNavigate();
-    const [suggestions, setSuggestions] = useState([]);
-    useEffect(() => {
-      axios.get(`http://localhost:9002/suggestions/${props.email}`)
-        .then(response => {
-          setSuggestions(response.data);
-        })
+const API_KEY = '84edaeedd68b9e73abbe95b5bb70617a';
+const Base_URL = 'https://api.themoviedb.org/3';
+
+const Suggestion = (props) => {
+  const navigate = useNavigate();
+  const [suggestions, setSuggestions] = useState([]);
+  const [movieDetails, setMovieDetails] = useState([]);
+  const [movieIds, setMovieIds] = useState([]);
+
+
+  useEffect(() => {
+    axios.get(`http://localhost:9002/suggestions/${props.email}`)
+      .then(response => {
+        setSuggestions(response.data);
+        // Extract movie IDs from suggestions
+        const movieIds = response.data.map(suggestion => suggestion.MovieId);
+        setMovieIds(movieIds);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [props.email]);
+
+  useEffect(() => {
+    const fetchMovieDetails = () => {
+      const promises = movieIds.map(movieId => {
+        return axios.get(`${Base_URL}/movie/${movieId}?api_key=${API_KEY}`);
+      });
+
+      axios.all(promises)
+        .then(axios.spread((...responses) => {
+          const movieDetails = responses.map(response => response.data);
+          setMovieDetails(movieDetails);
+        }))
         .catch(err => {
           console.log(err);
         });
-    }, []);
+    };
 
-    return(
-        <div>
-        <ul>
-        {suggestions.map((suggestion, index) => (
-          <li key={index}><p onClick={()=>navigate(`/searchMoviebyName/${suggestion.MovieId}`)}>{suggestion.MovieId} suggested by {suggestion.sender}</p></li>
+    if (movieIds.length > 0) {
+      fetchMovieDetails();
+    }
+  }, [movieIds]);
+
+  return (
+    <div className='Watchlist'>
+      <ul>
+        {movieDetails.map((movieDetail, index) => (
+          <li key={index}>
+            <div className="movie-card" >
+            <img src={`https://image.tmdb.org/t/p/w440_and_h660_face/${movieDetail.poster_path}`} alt={movieDetail.title} />
+              <div className="movie-details">
+                <h1 className="movie-title">{movieDetail.title}</h1>
+                <p onClick={() => navigate(`/searchMoviebyName/${movieDetail.id}`)}>More details</p>
+                <p>Suggested by {suggestions[index].sender}</p>
+              </div>
+            </div>
+          </li>
         ))}
       </ul>
-        </div>
-    )
+    </div>
+  );
 }
 
 export default Suggestion;
