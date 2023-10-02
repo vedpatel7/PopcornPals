a96422176744ebf03f43e0df92f2fcf1d202b0df
vedpatel7 <vedvirpara2004@gmail.com>
2023-10-02 10:45:35 +0530

    added loading screen

--- client/src/components/homepage/homepage.js
+++ client/src/components/homepage/homepage.js
@@ -8,6 +8,7 @@ import SearchMovie from "./SearchMovie";
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Input, InputRightElement, InputGroup } from '@chakra-ui/react'
 import NavBar from "../navbar/navbar";
+import { Spinner } from '@chakra-ui/react'
 
 const API_KEY = '84edaeedd68b9e73abbe95b5bb70617a';
 const Base_URL = 'https://api.themoviedb.org/3';
@@ -20,6 +21,7 @@ const Homepage = ({ setLoginUser }) => {
   const [popularmovies, setPopularMovies] = useState([]);
   const [upcomingmovies, setUpcomingMovies] = useState([]);
   const [playingmovies, setPlayingMovies] = useState([]);
+  const [loading, setLoading] = useState(true); // Add a loading state
 
   const navigate = useNavigate();
 
@@ -33,12 +35,14 @@ const Homepage = ({ setLoginUser }) => {
     navigate(`/watchlist/${emailId}`);
   }
 
-  const hadleSuggestion=()=>{
+  const hadleSuggestion = () => {
     const emailId = localStorage.getItem('EmailId');
     navigate(`suggestion/${emailId}`);
   }
 
   useEffect(() => {
+    // Initially, set loading to true
+    setLoading(true);
 
     axios.get(Popular)
       .then(response => {
@@ -47,6 +51,7 @@ const Homepage = ({ setLoginUser }) => {
       .catch(err => {
         console.log(err);
       });
+
     axios.get(Upcoming)
       .then(response => {
         setUpcomingMovies(response.data.results);
@@ -54,12 +59,16 @@ const Homepage = ({ setLoginUser }) => {
       .catch(err => {
         console.log(err);
       });
+
     axios.get(Now_Playing)
       .then(response => {
         setPlayingMovies(response.data.results);
       })
       .catch(err => {
         console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
 
   }, []);
@@ -68,43 +77,55 @@ const Homepage = ({ setLoginUser }) => {
     <div>
       <div className="search-bars">
         <SearchUser />
-        <SearchMovie/>
-      {/* <Button colorScheme= 'pink' onClick={handleLogout}>Logout</Button>
-      <Button colorScheme='pink' onClick={handleWatchList}>WatchList</Button>
-      <Button colorScheme='pink' onClick={hadleSuggestion}>Suggestions</Button> */}
-      </div>
-      <h1 className="trending-title">Trending Movies</h1>
-      <div className="carousel-container">
-        <div className="carousel">
-          {popularmovies.map((movie, index) => (
-            <div key={index} className="carousel-item">
-              <MovieCard {...movie} />
-            </div>
-          ))}
-        </div>
+        <SearchMovie />
       </div>
 
-      <h1 className="trending-title">Upcoming Movies</h1>
-      <div className="carousel-container">
-        <div className="carousel">
-          {upcomingmovies.map((movie, index) => (
-            <div key={index} className="carousel-item">
-              <MovieCard {...movie} />
+      {loading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        ><Spinner color='white.500' size='xl' /></div>
+      ) : (
+        <>
+          <h1 className="trending-title">Trending Movies</h1>
+          <div className="carousel-container">
+            <div className="carousel">
+              {popularmovies.map((movie, index) => (
+                <div key={index} className="carousel-item">
+                  <MovieCard {...movie} />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
-      <h1 className="trending-title">Now Playing Movies</h1>
-      <div className="carousel-container">
-        <div className="carousel">
-          {playingmovies.map((movie, index) => (
-            <div key={index} className="carousel-item">
-              <MovieCard {...movie} />
+          </div>
+
+          <h1 className="trending-title">Upcoming Movies</h1>
+          <div className="carousel-container">
+            <div className="carousel">
+              {upcomingmovies.map((movie, index) => (
+                <div key={index} className="carousel-item">
+                  <MovieCard {...movie} />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+          <h1 className="trending-title">Now Playing Movies</h1>
+          <div className="carousel-container">
+            <div className="carousel">
+              {playingmovies.map((movie, index) => (
+                <div key={index} className="carousel-item">
+                  <MovieCard {...movie} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
+
 export default Homepage;
